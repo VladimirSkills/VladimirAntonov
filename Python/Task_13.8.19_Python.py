dd627e0c87d0d187bae3c746872bc3c2b8d9f681
VladimirSkills <pob_1@mail.ru>
2022-04-29 03:24:41 +0400

    Add Task 13.8.19

---
+++ Python/Task_13.8.19_Python.py
@@ -0,0 +1,28 @@
+ticket = int(input('Укажите кол-во билетов (макс 5 шт): '))
+if 0 < ticket <= 5: # ограничение ввода по количеству
+    price = 0
+    for i in range(ticket):
+        while True:
+            try:
+                age = int(input('Возраст посетителя: '))
+                # Если вводится не число, будет вызвано исключение:
+            except ValueError:
+                # Цикл будет повторяться до правильного ввода:
+                print("Нужно ввести число! Попробуйте снова...")
+            else:
+                if age < 18:
+                    price += 0
+                elif 18 <= age < 25:
+                    price += 990
+                else:
+                    price += 1390
+                break
+    if ticket > 3:
+        discount = 0.9
+    else:
+        discount = 1
+    print('Стоимость билетов на', ticket, 'чел:', round(discount * price), 'руб.')
+
+else:
+    print('Один может купить не более 5 билетов!')
+
