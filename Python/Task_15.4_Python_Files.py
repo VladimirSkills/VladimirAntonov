366a7f40903e48b01d70142af42d1048f3260a11
VladimirSkills <pob_1@mail.ru>
2022-05-12 13:34:58 +0400

    Add Task 15.4 _ II

--- Python/Task_15.4_Python_Files.py
+++ Python/Task_15.4_Python_Files.py
@@ -9,7 +9,7 @@ import re
 
 filename = input('Укажите имя файла: ')
 ftype = input('Укажите тип файла (расширение): ')
-xfile = filename + '.' + ftype # <class 'str'> Файл txt: Testfile, из текущего каталога
+xfile = filename + '.' + ftype # <class 'str'> тип txt имя Testfile, из текущего каталога
 
 # Функция для поиска самого длинного англ. слова:
 def longWord(text):
@@ -21,22 +21,21 @@ def longWord(text):
 
 # открываем файл, читаем, приводим текст к нижнему регистру,
 # убираем знаки пунктуации, получаем список слов (цифры не учитываем)
-with open(xfile, encoding="utf8") as f: # , encoding="utf8"
+with open(xfile, encoding="utf8") as f:
     lst = f.read().lower().translate(
         str.maketrans('', '', '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~—')
     ).split()
 # составляем словарь подсчёта
 y = 3 # выводим слова длиною > y
 res = {x:lst.count(x) for x in lst if len(x) > y} # по кол-ву символов в слове
-
-print(f'Слова, длинною более {y}-х символов:')
-print(f'1-ое слово: "{max(res, key=(res.get))}" - {max(res.values())} шт') # первое из наиболее часто встречающихся
-
-#Модуль operator включает в себя функцию itemgetter(). Она возвращает значение словаря с ключом, который = 1:
+# Модуль operator включает в себя функцию itemgetter(). Она возвращает значение словаря с ключом, который = 1:
+# всё это сортируем по возрастанию....
 sorted_res = dict(sorted(res.items(), key=operator.itemgetter(1)))
-max2key = list(sorted_res.keys())[-2] # извлекаем 2-ой ключ с конца (2-ой максимум)
-max2val = operator.itemgetter(max2key) # конструкция для последующего извлечения значения 2-го ключа:
+print(f'Слова, длинною более {y}-х символов:')
+print(f'1-ое слово: "{max(sorted_res, key=(sorted_res.get))}" - {max(sorted_res.values())} шт') # первое из наиболее часто встречающихся
 
-print(f'2-ое слово: "{max2key}" - {max2val(sorted_res)} шт')
+sorted_res.pop(max(sorted_res, key=(sorted_res.get))) # Исключаем первый максимум...
+print(f'2-ое слово: "{max(sorted_res, key=(sorted_res.get))}" - {max(sorted_res.values())} шт')
+
 print(f'Самое длинное англ. слово: "{max(longWord(lst), key=len)}"')
 print(f'Самое длинное русское: "{", ".join([word for word in lst if len(word) == len(sorted(lst, key=len)[-1])]).lower()}"')
