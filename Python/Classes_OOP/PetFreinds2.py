67ff1e4b486f4d15d83b61d32ec11ac5500736f1
VladimirSkills <pob_1@mail.ru>
2022-05-15 23:19:18 +0400

    Add Task 16.7.1

--- Python/Classes_OOP/PetFreinds2.py
+++ Python/Classes_OOP/PetFreinds2.py
@@ -1,7 +1,9 @@
 from PetFreinds1 import Cat
 
-cat1 = Cat(name="Барон", gender="Мальчик", age=2)
-cat2 = Cat(name="Сэм", gender="Мужик", age=10)
+# cat1 = Cat(name="Барон", gender="Мальчик", age=2)
+# cat2 = Cat(name="Сэм", gender="Мужик", age=10)
+cat1 = Cat('Барон', 'Мальчик', 2)
+cat2 = Cat('Сэм', 'Мужик', 10)
 
 print(f"Имя: {cat1.name}, Пол: {cat1.gender}, Возраст: {cat1.age}")
 print(f"Имя: {cat2.name}, Пол: {cat2.gender}, Возраст: {cat2.age}")
