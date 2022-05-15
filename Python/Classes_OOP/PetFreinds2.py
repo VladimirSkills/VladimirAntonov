06ee43016c2e45abe5ac1a09f27a218022a56cf5
VladimirSkills <pob_1@mail.ru>
2022-05-15 23:05:22 +0400

    Add Task 16.7.1

---
+++ Python/Classes_OOP/PetFreinds2.py
@@ -0,0 +1,17 @@
+from PetFreinds1 import Cat
+
+cat1 = Cat(name="Барон", gender="Мальчик", age=2)
+cat2 = Cat(name="Сэм", gender="Мужик", age=10)
+
+print(f"Имя: {cat1.name}, Пол: {cat1.gender}, Возраст: {cat1.age}")
+print(f"Имя: {cat2.name}, Пол: {cat2.gender}, Возраст: {cat2.age}")
+print('')
+print("Имя:", cat1.getName())
+print("Пол:", cat1.getGender())
+print("Возраст:", cat1.getAge())
+print('')
+print("getAll-1:", cat1.getAll())
+print("getAll-2:", cat2.getAll())
+
+
+
