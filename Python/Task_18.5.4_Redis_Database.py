699fb8e7bac4dd5878b27e05c3984537e2e1f01a
VladimirSkills <pob_1@mail.ru>
2022-06-03 01:14:44 +0400

    Add Task 18.5.4 Redis cache

---
+++ Python/Task_18.5.4_Redis_Database.py
@@ -0,0 +1,36 @@
+"""
+<><>REDIS<><>
+"""
+import redis
+import json
+
+cache = redis.Redis(
+    host= 'redis-_YOURS_.com',
+    port=_YOURS_,
+    password='_YOURS_'
+)
+
+# ШАГ 1: Создание словаря
+data = {
+    'Владислав': '79083679393',
+    'Сергей': '79272145115',
+    'Данил': '79276149119',
+    'Николай': '79376151441'
+}  # создаём словарь для записи
+
+# ШАГ 2: Запись в кэш и вывод
+cache.set('data', json.dumps(data))  # записываем в кеш в строку
+converted_data = json.loads(cache.get('data'))  # переводим данные из кэша обратно в словарь
+
+listname = ', '.join(list(map(str, converted_data.keys()))) # делаем читабельный вид
+print(f"Создан список контактов Ваших друзей: {listname}")
+names = input(f'Введите имя друга для получения телефона: ')
+
+print(f"Его телефон: +{converted_data.get(names)}")
+delit = input(f'Удалить контакты? y/n ')
+for i in delit:
+    if i == "y":
+        cache.delete('data')  # чистим кэш
+        print(cache.get('data'))
+    else:
+        print(f"Контакты сохранены: \n{converted_data}")
