8623fc6339a63f2fdcc9d7f0d843a6b6c463437f
VladimirSkills <pob_1@mail.ru>
2022-06-05 02:28:06 +0400

    Add Task 18.6. My First BOT

---
+++ Python/MyFirstBOT/exclusions.py
@@ -0,0 +1,43 @@
+import json
+import requests
+from decimal import Decimal, ROUND_HALF_UP
+from initialdata import keys
+
+def num_format(num_x): # числовой формат:
+    n = Decimal(str(num_x))
+    number = n.quantize(Decimal("1.00"), ROUND_HALF_UP)  # "1.00" - здесь задаём кол-во знаков после запятой
+    formi = '{0:,}'.format(number).replace(',', ' ')  # (',', ' ') - во вторых кавычках ставим нужный разделитель
+    return formi
+
+# Обработчик некорректного ввода:
+class ConvertionException(Exception):  # Ошибки пользователя
+    pass
+
+# Обработку ошибок выводим в отдельный класс с применением декоратора staticmethod к функции convert,
+# чтобы не перегружать работу бота:
+class CryptoConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:  # если введены одинаковые валюты:
+            raise ConvertionException(f"Вы ввели одинаковую или некорректную валюту {base}!")
+
+        # Обработка некорректного ввода имени валюты пользователем:
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:  # Ошибка по ключу
+            raise ConvertionException(f"Не удалось обработать валюту {quote}!")
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f"Не удалось обработать валюту {base}!")
+
+        # Обработка некорректного ввода количества конвертируемой валюты - amount:
+        try:
+            amount = float(amount)
+        except ValueError:  # Ошибка по значению
+            raise ConvertionException(f"Не удалось обработать количество {amount}!")
+
+        # вставляем с сайта запрос и делаем его динамическим, т.е. c изменяемыми переменными {код валюты по ключу}:
+        r = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}")
+        total_base = num_format(json.loads(r.content)[keys[base]] * amount)  # получаем конверт. значение
+        return total_base
---
+++ Python/MyFirstBOT/initialdata.py
@@ -0,0 +1,15 @@
+# Токен, полученный при регистрации БОТа в Telegramm через BotFather:
+TOKEN = "ввести token Telegram"
+
+# Список валют:
+keys = {
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR'
+}
+
+# подсчёт кол-ва единиц валюты:
+countcy = 0
+for key in keys.keys():
+    countcy += 1
+print(countcy)
