7fddcb42b4737920021cc85c9e61844d32a688b7
VladimirSkills <pob_1@mail.ru>
2022-06-15 01:22:58 +0400

    Add Task 19.4 My requests

--- Python/Task_19.4_My_requests.py
+++ Python/Task_19.4_My_requests.py
@@ -50,11 +50,11 @@ def post_add_pet(auth_key: json) -> json:
 print(post_add_pet(auth_key_x))
 
 
-# Добавление питомца с фото
+# Добавление питомца с фото №1
 # Используем библиотеку requests_toolbelt, чтобы устранить проблему с загрузкой изображения на сервер:
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
-def add_new_petfoto(auth_key: json, name: str, animal_type: str,
+def add_new_petfoto1(auth_key: json, name: str, animal_type: str,
                 age: str, pet_photo: str) -> json:
 
     data = MultipartEncoder(
@@ -75,7 +75,27 @@ def add_new_petfoto(auth_key: json, name: str, animal_type: str,
         result = res.text
     return status, result
 
-print(add_new_petfoto(auth_key_x, name='King-Kong', animal_type='Monkey', age='1', pet_photo='images/king-kong1.jpg'))
+print(add_new_petfoto1(auth_key_x, name='King-Kong', animal_type='Monkey', age='1', pet_photo='images/king-kong1.jpg'))
+
+
+# Добавление питомца с фото №2
+def add_new_petfoto2(auth_key: json) -> json:
+
+    headers = {
+        'auth_key': auth_key,
+    }
+    data = {'name': 'King-Kongs', 'animal_type': 'Monkey', 'age': 155}
+    files = {"pet_photo": open("images/king-kong2.jpg", "rb")}
+    base_url = 'https://petfriends.skillfactory.ru/'
+    res = requests.post(base_url + 'api/pets', data=data, files=files, headers=headers)
+    status = res.status_code
+    try:
+        result = res.json()
+    except json.decoder.JSONDecodeError:
+        result = res.text
+    return status, result
+
+print(add_new_petfoto2(auth_key_x))
 
 
 # Добавление/изменение фото
