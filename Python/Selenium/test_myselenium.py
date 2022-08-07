47b20dcf4d105d940f5d2ec6d09602c2d77926d2
VladimirSkills <pob_1@mail.ru>
2022-08-07 12:34:45 +0400

    Selenium find_elements PyTest + LogIn & Cookies

--- Python/Selenium/test_myselenium.py
+++ Python/Selenium/test_myselenium.py
@@ -368,3 +368,58 @@ def test_cookie_petfriends(web_browser):
     web_browser.save_screenshot('All pets.png')
     # Ну и проверяем...
     assert page.get_current_url() == 'https://petfriends.skillfactory.ru/all_pets'
+
+
+
+
+"""Сделал парсинг-тест студентам
+для разбора примера проверки наличия элементов в карточках, загруженных пользователями..."""
+
+"""Команда для запуска, если chromedriver.exe находится в папке с тестом и путь в Terminal также к этой папке указан:
+pytest -v --driver Chrome --driver-path chromedriver.exe test_ВАШЕ ИМЯ.py::test_show_my_pets
+Данный тест, можно запустить и через Run (только закомитьте другие тесты в файле)"""
+
+
+@pytest.fixture(autouse=True)
+def browser():
+    with webdriver.Chrome() as driver:  # with - закрывает браузер в т.ч. при ошибке
+        # Переходим на страницу авторизации
+        driver.get('http://petfriends.skillfactory.ru/login')
+
+        yield driver
+        driver.quit()
+
+
+def test_show_my_pets(browser):  # фикстуру добавляем в аргумент функции
+    driver = browser  # транспортируем драйвер из фикстуры
+    # Вводим email
+    driver.find_element(By.ID, 'email').send_keys(email)
+    # Вводим пароль
+    driver.find_element(By.ID, 'pass').send_keys(password)
+    time.sleep(2)
+    # Нажимаем на кнопку входа в аккаунт
+    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    # Проверяем, что мы оказались на главной странице пользователя
+    assert driver.find_element(By.TAG_NAME, 'h1').text == "PetFriends"
+    time.sleep(2)
+
+    """
+    Мы объявили три переменные, в которых записали все найденные элементы на странице:
+    в images — все картинки питомцев, в names — все их имена, в descriptions — все виды и возрасты.
+    """
+    images = driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-img-top')
+    names = driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-title')
+    descriptions = driver.find_elements(By.CSS_SELECTOR, '.card-deck .card-text')
+
+    for i in range(len(names)):
+        assert images[i].get_attribute('src') != ''  # на странице нет питомцев без фото
+        assert names[i].text != ''  # на странице нет питомцев без Имени
+        assert descriptions[i].text != ''  # на странице нет питомцев с пустым полем для указания Породы и возраста
+        assert ', ' in descriptions[i]  # проверяем, что между породой и лет есть запятая (значит есть оба значения)
+        parts = descriptions[i].text.split(", ")  # Создаём список, где разделитель значений - запятая
+        assert len(parts[0]) > 0  # Проверяем, что длина текста в первой части списка и
+        assert len(parts[1]) > 0  # ...и во второй > 0, значит там что-то да указано! Если нет -> FAILED!
+
+    assert driver.find_element(By.TAG_NAME, 'h1').text == "PetFriends"  # проверка, что мы были на главной странице
+    # есть утверждение для проверки заголовка страницы, что <title> Label содержит текст «PetFriends»:
+    assert 'PetFriends' in driver.title
