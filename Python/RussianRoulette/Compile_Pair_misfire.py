ce1fcfdec81c377428cb7fe3e2ab42a34d6def6d
VladimirSkills <pob_1@mail.ru>
2023-11-12 17:57:30 +0400

    Russian Roulette GAME + Compile

--- Python/RussianRoulette/Compile_Pair_misfire.py
+++ Python/RussianRoulette/Compile_Pair_misfire.py
@@ -12,12 +12,16 @@ def gunshot_sounds(sound_path):
     play_obj.wait_done()
 
 
+# pyinstaller -i D:\Папа\SkillFactory\hat_magic.ico Compile_Pair_misfire.py -F
 def russian_roulette_pair_misfire():
     """Nuitka — компилятор Python. Установка: pip install Nuitka
-    Команда для компиляции в файл: python -m nuitka --follow-imports Compile_Pair_misfire.py"""
+    Команда для компиляции в файл.exe: python -m nuitka --follow-imports Compile_Pair_misfire.py"""
+    first_shooter = input(f"{Fore.LIGHTCYAN_EX}Введите Nickname ПЕРВОГО стрелка: ")
+    second_shooter = input(f"{Fore.LIGHTBLUE_EX}Введите Nickname ВТОРОГО стрелка: ")
     while True:
         try:
             shooter_choice = int(input(f"{Fore.LIGHTWHITE_EX}Выберите число патронов от 1 до 6: "))
+
             if 1 <= shooter_choice <= 6:
                 bullet_chamber_numbers = random.sample(range(1, 7), shooter_choice)
                 chambers = ', '.join(map(str, sorted(bullet_chamber_numbers)))
@@ -27,6 +31,7 @@ def russian_roulette_pair_misfire():
                 else:
                     misfire = random.choice(bullet_chamber_numbers)
                 print(Fore.LIGHTGREEN_EX, f"Патроны вложены в патронники №: {chambers}", sep='')
+                time.sleep(2)
                 if shooter_choice == 6:
                     print(Fore.LIGHTYELLOW_EX, "Господа, Вас спасёт только Осечка! :)", sep='')
                     time.sleep(2)
@@ -34,34 +39,35 @@ def russian_roulette_pair_misfire():
                 count = 1
                 bullet = 0
                 while True:
-                    print(Fore.RESET, "\nКрутим барабан...", sep='')
-                    time.sleep(1)
+                    print(Fore.RESET, "\nКрутим барабан... ", sep='')
                     if count % 2 != 0:
-                        print(Fore.LIGHTCYAN_EX, f"Стрелок 1 спускает курок!", sep='')
+                        print(Fore.LIGHTCYAN_EX, f"Стрелок '{first_shooter.upper()}' спускает курок! ╦═━^━", sep='')
+                        time.sleep(1)
                     else:
-                        print(Fore.LIGHTBLUE_EX, f"Стрелок 2 спускает курок!", sep='')
+                        print(Fore.LIGHTBLUE_EX, f"Стрелок '{second_shooter.upper()}' спускает курок! ╦═━^━", sep='')
+                        time.sleep(1)
                     shot = random.choice(range(1, 7))
                     if shot in bullet_chamber_numbers and shot != misfire + bullet:
                         print(Fore.LIGHTRED_EX, f"Выстрел! [\u25cf] || Патронник № {shot}", sep='')
-                        time.sleep(0.5)
                         gunshot_sounds('sounds/shot.wav')
+                        time.sleep(0.5)
                         break
                     elif shot == misfire and int(misfire) + bullet in bullet_chamber_numbers:
                         print(Fore.LIGHTMAGENTA_EX, f"Осечка!!! [\u2665] || Патронник № {shot}", sep='')
-                        time.sleep(0.5)
                         gunshot_sounds('sounds/misfire.wav')
+                        time.sleep(0.5)
                         bullet += 6
                     else:
                         print(Fore.LIGHTYELLOW_EX, f"Пустой! ~\u263B~ || Патронник № {shot}", sep='')
-                        time.sleep(0.5)
                         gunshot_sounds('sounds/empty.wav')
+                        time.sleep(0.5)
                     count += 1
-                print(Fore.LIGHTWHITE_EX, f"\n->\u2573<- Конец игры на {count} выстреле!\n", sep='')
+                print(Fore.LIGHTWHITE_EX, f"\n<╦═━^━ Конец игры на {count} выстреле!\n", sep='')
                 print(Fore.YELLOW, f"@ https://t.me/amusing_python", sep='')
-                print(input(f"{Fore.WHITE}[!] Для выхода нажмите Enter ->"), sep='')
+                # print(input(f"{Fore.WHITE}[!] Для выхода нажмите Enter ->"), sep='')
                 break
         except ValueError:
-            print("[INFO] Нужно ввести количество патронов от 1 до 6!")
+            print(Fore.LIGHTYELLOW_EX, "[INFO] Нужно ввести количество патронов от 1 до 6!", sep='')
 
 
 russian_roulette_pair_misfire()
